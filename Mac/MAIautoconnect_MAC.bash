5b3406890d4cdf3ca2e7bb48b2beb09d310dd44b
plaguedo <voevodov97@mail.ru>
2016-09-30 12:02:11 +0300

    Fixed some bugs

--- Mac/MAIautoconnect_MAC.bash
+++ Mac/MAIautoconnect_MAC.bash
@@ -13,7 +13,7 @@ save_dir=~/.scriptsByCorruptedProject/
 
 uninstall () {
     rm -R $save_dir || {
-        echo "Can't uninstall. Something go wrong."
+        echo "Can't be uninstalled. Something went wrong."
         exit 66
     }
     echo "All trash has been deleted."
@@ -28,14 +28,14 @@ else
         echo $VERSION
         exit 0
     elif [[ $command == "--help" || $command == "?" || $command == "help" || $command == "h" ]]; then
-        echo "Just simple script for autoconnect to $purpose_SSID"
+        echo "Just a simple script that autoconnects to $purpose_SSID"
         echo "You can use next flags:"
-        echo "    i[nstall] - just add this script to cron for autostart and make ~/.scripts directory"
-        echo "    u[ninstall] - uninstall this script from system"
-        echo "    h[elp] - will show this message"
-        echo "    v[ersion] - will show current version"
-        echo -e "\nYou must know, that script use crontab and networksetup, if those utilits didn't install on your PC it willn't work!"
-        echo "Before install script save your own crontab tasks in $save_dir/oldcron, but after uninstall didn't clear current tasks."
+        echo "    i[nstall] - adds this script to cron for autostart and make $save_dir directory"
+        echo "    u[ninstall] - uninstalls this script from system"
+        echo "    h[elp] - shows this message"
+        echo "    v[ersion] - shows current version"
+        echo -e "\nThis script use crontab and networksetup. If those utilits aren't installed on your PC script won't work!"
+        echo "Before script installs itself, it saves your own crontab tasks in $save_dir/oldcron. Warning! After the deinstallation settings won't be deleted!"
         exit 0
     elif [[ $command == "uninstall" || $command == "u" ]]; then
         uninstall
@@ -45,29 +45,29 @@ else
         #save old cron tasks
         oldcrontask=$(crontab -l)
         if [[ $? > 1 ]]; then #check exit code for last command (crontab -l)
-            echo "Crontab's task hasn't been saw. Something go wrong. [2 step]"
+            echo "Crontab's task hasn't been shown. Something went wrong. [2 step]"
             uninstall
             exit 2
         fi
         #hidden dir
         ( mkdir $save_dir; cp $0 $save_dir/ ) || {
-            echo "Script hasn't been installed. Something go wrong. [1 step]"
+            echo "Script hasn't been installed. Something went wrong. [1 step]"
             uninstall
             exit 1
         }
         #cron again
         ( echo -e "$oldcrontask" > $save_dir/oldcron) || {
-            echo "Crontab's task hasn't been saved. Something go wrong. [3 step]"
+            echo "Crontab's task hasn't been saved. Something went wrong. [3 step]"
             uninstall
             exit 3
         }
-        ( echo -e "$oldcrontask \nMAILTO=''\n@reboot $save_dir$0\n" | crontab - ) || {
-            echo "Crontab's task hasn't been update. Something go wrong. [4 step]"
+        ( echo -e "$oldcrontask \nMAILTO=''\n@reboot $save_dir$0 > /dev/null\n" | crontab - ) || {
+            echo "Crontab's task hasn't been updated. Something went wrong. [4 step]"
             uninstall
             exit 4
         }
         nohup $save_dir$0 0>/dev/null 1>/dev/null 2>/dev/null &
-        echo "Script has been installed. Autoconnect - start!"
+        echo "Script has been installed. Autoconnect start!"
         exit 0
     fi
 fi
@@ -80,7 +80,8 @@ do
     if [[ $SSID == $purpose_SSID ]]; then
         echo "Correct SSID."
         echo "Connecting..."
-        curl "1.1.1.1/login.html?network_name=Guest+Network&username=mai&password=1930&buttonClicked=4"
+        curl "1.1.1.1/login.html?network_name=Guest+Network&username=mai&password=1930&buttonClicked=4" > /dev/null
+	echo "Request has been sent."
     else
         echo "Another SSID"
         echo $SSID
